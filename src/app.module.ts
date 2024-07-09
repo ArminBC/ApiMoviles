fdf65165bd730a56900aef90e1a05bf9438ec420
Armin Borges Cob <116670315+ArminBC@users.noreply.github.com>
2024-07-09 09:38:39 -0600

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -18,7 +18,7 @@ import { Transference } from './transferences/entities/transference.entity';
 const Database: TypeOrmModuleOptions = {
   type: 'mysql',
   host: 'localhost',
-  port: 3307,
+  port: 3306,
   username: 'root',
   password: 'root',
   database: 'central_bank',
