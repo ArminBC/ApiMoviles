3179b675b7909a29cef7bdd26e4091bc3361d2ca
Armin Borges Cob <116670315+ArminBC@users.noreply.github.com>
2024-07-09 19:51:58 -0500

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,7 @@ import { CustomValidationPipe } from './validation/CustomValidationPipe';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new CustomValidationPipe());
+  app.enableCors({origin: true, methods: "*", allowedHeaders: "*"});
   await app.listen(3000);
 }
 bootstrap();
